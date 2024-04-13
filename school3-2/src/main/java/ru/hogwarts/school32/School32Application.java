1fd9e73a38aea1451eaa2368e37b8157a8150aed
KosbakovIsmail <kosbakovismail@gmail.com>
2024-04-13 12:22:33 +0300

    initial commit

---
+++ school3-2/src/main/java/ru/hogwarts/school32/School32Application.java
@@ -0,0 +1,15 @@
+package ru.hogwarts.school32;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class School32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(School32Application.class, args);
+	}
+
+}
